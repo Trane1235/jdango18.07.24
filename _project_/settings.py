7ceb0802fd12c9fba7283abb392318bb6ada9ec1
EOV <sweet16102019@gmail.com>
2024-08-06 19:41:57 +0400

    1

--- _project_/settings.py
+++ _project_/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'apps.users',
-    'apps.book'
+
 ]
 
 MIDDLEWARE = [
--- apps/book/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.UserLoginView.as_view(), name='users_login'),
-    path('register/', views.UserRegisterView.as_view(), name='user_register')
-]
